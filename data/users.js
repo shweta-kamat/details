0c7da2847a3c17b370f1a5df91675e5c0382ab76
shweta-kamat <56191405+shweta-kamat@users.noreply.github.com>
2019-10-05 16:00:24 +0530

    Add files via upload

---
+++ data/users.js
@@ -0,0 +1,13 @@
+let users =  [
+    {
+      id: 1,      
+      name:"Shweta",
+      surname:"kamat",
+      title: "Sr Software Engineer",
+      description: "Node js developer",
+
+    }
+];
+
+export default users;
+
